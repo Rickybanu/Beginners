a72821037cc41d6cf50cc4a715ac90fd4ffe5ccd
Banu Prakash <m.banuprakashmib@gmail.com>
2018-03-20 22:04:04 +0530

    Update vowel_or_not.py

--- twst/beg/vowel_or_not.py
+++ twst/beg/vowel_or_not.py
@@ -1,6 +1,6 @@
-ch = raw_input()
-if(ch=='A' or ch=='a' or ch=='E' or ch =='e' or ch=='I'
- or ch=='i' or ch=='O' or ch=='o' or ch=='U' or ch=='u'):
+v = raw_input()
+if(v=='A' or v=='a' or v=='E' or v =='e' or v=='I'
+ or v=='i' or v=='O' or v=='o' or v=='U' or v=='u'):
     print("Vowel")
 else:
     print("Consonant")
